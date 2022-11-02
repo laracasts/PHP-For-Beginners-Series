effe8c92aee49e11d899ee92742634ca8d752caa
Jeffrey Way <jeffrey@jeffrey-way.com>
2022-11-02 11:43:01 -0400

    wip

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+
+// Connect to the MySQL database.
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+// Tip: This should be wrapped in a try-catch. We'll learn how, soon.
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts where id = 1");
+$statement->execute();
+
+$post = $statement->fetch(PDO::FETCH_ASSOC);
+
+echo "<li>" . $post['title'] . "</li>";
