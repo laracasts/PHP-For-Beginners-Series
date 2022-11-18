b2c46bea09d70855e33184869126897d6dcda98a
Jeffrey Way <jeffrey@jeffrey-way.com>
2022-11-18 14:40:22 -0500

    wip

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <?php
 
 require 'functions.php';
-//require 'router.php';
+require 'router.php';
 require 'Database.php';
 
 $config = require('config.php');
 $db = new Database($config['database']);
 
-$id = $_GET['id'];
-$query = "select * from posts where id = :id";
-
-$posts = $db->query($query, [':id' => $id])->fetch();
-
-dd($posts);
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -32,25 +32,6 @@
                                 <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
                             </button>
                         </div>
-
-                        <!--
-                          Dropdown menu, show/hide based on menu state.
-
-                          Entering: "transition ease-out duration-100"
-                            From: "transform opacity-0 scale-95"
-                            To: "transform opacity-100 scale-100"
-                          Leaving: "transition ease-in duration-75"
-                            From: "transform opacity-100 scale-100"
-                            To: "transform opacity-0 scale-95"
-                        -->
-                        <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                            <!-- Active: "bg-gray-100", Not Active: "" -->
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
-                        </div>
                     </div>
                 </div>
             </div>
