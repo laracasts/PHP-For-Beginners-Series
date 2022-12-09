9d1892fc479d8574888abe6b5531d3d52abd462f
Jeffrey Way <jeffrey@jeffrey-way.com>
2022-12-09 13:17:52 -0500

    Episode 27

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,10 +6,22 @@ $db = new Database($config['database']);
 $heading = 'Create Note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ]);
+    $errors = [];
+
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body is required';
+    }
+
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'The body can not be more than 1,000 characters.';
+    }
+
+    if (empty($errors)) {
+        $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
 }
 
 require 'views/note-create.view.php';
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -10,19 +10,32 @@
                     <div class="shadow sm:overflow-hidden sm:rounded-md">
                         <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
                             <div>
-                                <label for="body" class="block text-sm font-medium text-gray-700">Body</label>
+                                <label
+                                    for="body"
+                                    class="block text-sm font-medium text-gray-700"
+                                >Body</label>
 
                                 <div class="mt-1">
-                                    <textarea id="body" name="body" rows="3"
-                                              class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                              placeholder="Here's an idea for a note..."></textarea>
+                                    <textarea
+                                        id="body"
+                                        name="body"
+                                        rows="3"
+                                        class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                        placeholder="Here's an idea for a note..."
+                                    ><?= $_POST['body'] ?? '' ?></textarea>
+
+                                    <?php if (isset($errors['body'])) : ?>
+                                        <p class="text-red-500 text-xs mt-2"><?= $errors['body'] ?></p>
+                                    <?php endif; ?>
                                 </div>
                             </div>
                         </div>
 
                         <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                            <button type="submit"
-                                    class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                            <button
+                                type="submit"
+                                class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            >
                                 Save
                             </button>
                         </div>
