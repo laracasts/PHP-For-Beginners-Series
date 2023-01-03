0278ea15ca706fa0cabbd81fdbe7369701aaa786
Jeffrey Way <jeffrey@jeffrey-way.com>
2023-01-03 09:40:15 -0500

    wip

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -6,7 +6,6 @@ $db = new Database($config['database']);
 $errors = [];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
     if (! Validator::string($_POST['body'], 1, 1000)) {
         $errors['body'] = 'A body of no more than 1,000 characters is required.';
     }
--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ const BASE_PATH = __DIR__.'/../';
 require BASE_PATH.'functions.php';
 
 spl_autoload_register(function ($class) {
-    require base_path("Core/" . $class . '.php');
+    require base_path("Core/{$class}.php");
 });
 
 require base_path('router.php');
