e7c9e6accb0aedbe968710bef22a007186ff79db
Muhammed Osman <faddel.homsi@gmail.com>
2023-01-07 22:34:30 +0300

    First Step

--- config.php
+++ config.php
@@ -4,9 +4,8 @@ return [
     'database' => [
         'host' => 'localhost',
         'port' => 3306,
-        'dbname' => 'myapp',
-        'charset' => 'utf8mb4'
+        'dbname' => 'phpseries',
+        'charset' => 'utf8'
     ],
 
-    //
 ];
