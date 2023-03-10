9c111c8c714d73ccdc38b1d4ad5af48952992816
Jeffrey Way <jeffrey@jeffrey-way.com>
2023-03-10 13:07:53 -0500

    Episode 40

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -34,7 +34,7 @@ if ($user) {
 } else {
     $db->query('INSERT INTO users(email, password) VALUES(:email, :password)', [
         'email' => $email,
-        'password' => $password // NEVER store database passwords in clear text. We'll fix this in the login form episode. :)
+        'password' => password_hash($password, PASSWORD_BCRYPT)
     ]);
 
     $_SESSION['user'] = [
--- views/index.view.php
+++ views/index.view.php
@@ -4,7 +4,7 @@
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <p>Hello. Welcome to the home page.</p>
+        <p>Hello, <?= $_SESSION['user']['email'] ?? 'Guest' ?>. Welcome to the home page.</p>
     </div>
 </main>
 
