74f46e51243247b36efea90d88afe5c6fe4ad5c0
Jeffrey Way <jeffrey@jeffrey-way.com>
2023-04-26 14:43:21 -0400

    Episode 47

--- public/index.php
+++ public/index.php
@@ -3,18 +3,12 @@
 use Core\Session;
 use Core\ValidationException;
 
-session_start();
-
 const BASE_PATH = __DIR__.'/../';
 
+session_start();
+
+require BASE_PATH . '/vendor/autoload.php';
 require BASE_PATH.'Core/functions.php';
-
-spl_autoload_register(function ($class) {
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-
-    require base_path("{$class}.php");
-});
-
 require base_path('bootstrap.php');
 
 $router = new \Core\Router();
